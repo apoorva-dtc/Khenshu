efedb463341a25d5eda63f52fe414a9bcad391b1
apoorva-dtc <apoorva.g@dtcinfotech.com>
2023-02-17 17:25:26 +0530

    initial commit

---
+++ src/components/Thankyou/Thankyou.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../Thankyou/Thankyou.css";
+
+function Thankyou() {
+    return (
+        <div className='Success-msg'>
+            Thanks for submiting the details. <br />
+            Khenshu team will get in touch with you shortly.
+        </div>
+    )
+}
+
+export default Thankyou;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import NotFound from "../components/NotFound/NotFound.js"
+
+function NotFound() {
+  return <NotFound />;
+}
+
+export default NotFound;
